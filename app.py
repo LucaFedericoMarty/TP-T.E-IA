8bdc71b3a0cef4cf11911ca63fd93724b4d8652a
LucaFedericoMarty <101400526+LucaFedericoMarty@users.noreply.github.com>
2022-11-12 20:53:51 -0300

    app deploy

--- app.py
+++ app.py
@@ -1,41 +1,78 @@
 import streamlit as st
-
+from PIL import Image
+from keras.preprocessing.image import ImageDataGenerator
+from numpy import expand_dims
+from tensorflow.keras.utils import load_img
+from tensorflow.keras.utils import img_to_array
+from keras.models import Sequential, load_model
+from tensorflow import keras
+from tensorflow.keras import layers, callbacks
+from tensorflow.keras.models import Model, Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout, Input, Activation
 
 with st.container():
     st.title("Trabajo Practico IA Deployement")
     st.write("Esta es la pagina de pruebas para nuestro IA")
 
-thr = st.sidebar.slider("Detection Threshold", min_value = 0.0, max_value = 1.0, value = 0.3, step = 0.01)
+class_names = ['Bean', 'Bitter_Gourd', 'Bottle_Gourd', 'Brinjal', 'Broccoli', 'Cabbage', 'Capsicum', 'Carrot', 'Cauliflower', 'Cucumber', 'Papaya', 'Potato', 'Pumpkin', 'Radish', 'Tomato']
 
-# model = st.sidebar.selectbox("Select Model",  ({"EfficientDet0" : 'logs/model.tflite'}, {"EfficientDet1" : ': logs/model1.tflite'}))
+model = Sequential()
+model.add(Conv2D(filters = 32, kernel_size = (3, 3), input_shape=(256, 256, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-image_file = st.file_uploader("Upload images for object detection", type=['png','jpeg'])
+model.add(Conv2D(filters = 32, kernel_size = (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model = st.file_uploader("Model", type=['tflite'])
+model.add(Conv2D(filters = 64, kernel_size = (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors 
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation = 'softmax'))
+
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+path_best_model = 'best_model.hdf5'
+
+model.load_weights(path_best_model)
+
+image_file = st.file_uploader("Upload images for vegetable classification", type=['png','jpeg'])
+
+def predict(image):
+    img = load_img(image, target_size = (256,256))
+
+    # La convierto a un numpy array
+    img = img_to_array(img)
+
+    # Le sumo una dimesion, cuyo valor es cero. 
+    # De (256, 256, 3) a (256, 256, 3, 0)
+    # Esto porque la funcion flow para cargar con el iterator la imagen necesita un array de 4 dimensiones como input
+    img = expand_dims(img, 0)
+
+    # ImageDataGenerator brillo
+    datagen = ImageDataGenerator(rescale=1./255)
+
+    test_img = datagen.flow(img)
+
+    predictions = model.predict(test_img)
+
+    number_class_pred = np.argmax(predictions)
+
+    prediction = class_names[number_class_pred]
+
+    return prediction
 
 if image_file is not None:
     input_image = Image.open(image_file)
     st.image(input_image)
 
-detect = st.button("Detect objects")
+detect = st.button("Classify")
 
 if detect:
-    cwd = os.getcwd()
-    # Change the test file path to your test image
-    im = Image.open(image_file)
-    im.thumbnail((512, 512), Image.ANTIALIAS)
-
-    # Load the TFLite model
-    interpreter = tf.lite.Interpreter(model_path="logs/model1.tflite")
-    interpreter.allocate_tensors()
-
-    # Run inference and draw detection result on the local copy of the original file
-    detection_result_image = run_odt_and_draw_results(
-      input_image,
-      interpreter,
-      threshold=thr
-)
-
-    # Show the detection result
-    img = Image.fromarray(detection_result_image)
-    st.image(img)
+    prediction = predict(image_file)
+    st.write(prediction)
